14c1af4e66000173ea01cdd385a2ab8af146a281
ViggyC <vich7304@colorado.edu>
2021-04-19 15:34:31 -0600

    dbconfig update

--- Code/Frontend/dbConfig.js
+++ Code/Frontend/dbConfig.js
@@ -6,10 +6,13 @@ const isProduction = process.env.NODE_ENV ==='production';
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 //port is 5433
+
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString
 });
 
+console.log("connection string: " + connectionString);
+console.log("is production: " + isProduction);
 module.exports = {pool};
 
 //the USER is optimizedhealth
