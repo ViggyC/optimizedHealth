577ee395d067a6e9a086aaadd4d7d4f1c23e80ef
ViggyC <vich7304@colorado.edu>
2021-04-19 16:06:36 -0600

    string

--- dbConfig.js
+++ dbConfig.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 
-const {Pool} = require("pg");
+const {Pool} = require('pg');
 
 const isProduction = process.env.NODE_ENV ==='production';
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+//const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 //port is 5433
 
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-});
+// const pool = new Pool({
+//     connectionString: process.env.NODE_ENV ==='production' ? process.env.DATABASE_URL : connectionString
+// });
+
+const connectionString= 'postgres://gpdaahccetqiqx:aa837e8b9ca07e3da45e94b09d6be62429edeab5e26558169deb8a09b0c2d35f@ec2-107-22-245-82.compute-1.amazonaws.com:5432/d6cp7nh1laguuu'
+
+const pool= new Pool({
+    connectionString: connectionString
+})
 
 console.log("connection string: " + connectionString);
 console.log("is production: " + isProduction);
