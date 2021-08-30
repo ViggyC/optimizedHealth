7a8d3484783bbe6a8e84f45b099db40e00de7b4b
ViggyC <vich7304@colorado.edu>
2021-08-29 18:22:08 -0600

    error message fix

--- dbConfig.js
+++ dbConfig.js
@@ -8,7 +8,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 //port is 5433
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString, ssl:{
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString, ssl: {
       rejectUnauthorized: false
     }
 });
